30112314ee1fc36fb2c20f5b0267638f1892d3ab
leo300000 <104022622+leo300000@users.noreply.github.com>
2025-01-24 17:40:30 +0100

    modif est finalisation atelier2

--- atelier2/etape2/gen_parquetnew.py
+++ atelier2/etape2/gen_parquetnew.py
@@ -3,18 +3,15 @@ import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
 
-# Fonction pour récupérer les données de l'API GitHub
 def fetch_data(url):
     response = requests.get(url)
     return response.json()
 
-# Fonction pour convertir les données en fichier Parquet
 def save_to_parquet(data, filename):
     df = pd.DataFrame(data)
     table = pa.Table.from_pandas(df)
     pq.write_table(table, filename)
 
-# URL de l'API pour chaque table
 urls = {
     "public.Temps": "https://api.github.com/repos/elizaOS/eliza/issues?state=all",
     "public.Activities": "https://api.github.com/repos/elizaOS/eliza/issues?state=all",
@@ -22,7 +19,6 @@ urls = {
     "Issue.Pull": "https://api.github.com/repos/elizaOS/eliza/pulls?state=all"
 }
 
-# Récupérer et sauvegarder les données pour chaque table
 for table_name, url in urls.items():
     data = fetch_data(url)
     save_to_parquet(data, f"{table_name}.parquet")
--- atelier2/etape3/load_datanews.py
+++ atelier2/etape3/load_datanews.py
@@ -1,33 +1,19 @@
-import requests
-import pandas as pd
-import pyarrow as pa
-import pyarrow.parquet as pq
 import duckdb
+import pyarrow.parquet as pq
+import os
 
-# Fonction pour récupérer les données de l'API GitHub
-def fetch_data(url):
-    response = requests.get(url)
-    return response.json()
+def load_parquet_to_duckdb(file_path, table_name, conn):
+    table = pq.read_table(file_path)
+    conn.execute(f"CREATE TABLE {table_name} AS SELECT * FROM table")
 
-# Fonction pour convertir les données en DataFrame
-def json_to_dataframe(data):
-    return pd.DataFrame(data)
+directory = '../etape2'
 
-# URL de l'API pour chaque table
-urls = {
-    "raw_Temps": "https://api.github.com/repos/elizaOS/eliza/issues?state=all",
-    "raw_Activities": "https://api.github.com/repos/elizaOS/eliza/issues?state=all",
-    "raw_Contributeurs": "https://api.github.com/repos/elizaOS/eliza/contributors",
-    "raw_Issue_Pull": "https://api.github.com/repos/elizaOS/eliza/pulls?state=all"
-}
-
-# Créer une connexion à la base DuckDB
 conn = duckdb.connect('eliza.db')
 
-# Récupérer et charger les données pour chaque table
-for table_name, url in urls.items():
-    data = fetch_data(url)
-    df = json_to_dataframe(data)
-    conn.execute(f"CREATE TABLE {table_name} AS SELECT * FROM df")
+for filename in os.listdir(directory):
+    if filename.endswith('.parquet'):
+        file_path = os.path.join(directory, filename)
+        table_name = f"raw_{os.path.splitext(filename)[0]}"
+        load_parquet_to_duckdb(file_path, table_name, conn)
 
-print("Les données ont été récupérées et chargées dans la base DuckDB.")
+print("Les fichiers Parquet ont été chargés dans la base DuckDB.")
